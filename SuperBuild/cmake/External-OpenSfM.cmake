2df561a93dc5f15271fcbdba4023493182ddb4be
Jadson Ribeiro <contato@jadsonbr.com.br>
2022-10-24 17:43:07 -0300

    Change repo OpenSFM

--- SuperBuild/cmake/External-OpenSfM.cmake
+++ SuperBuild/cmake/External-OpenSfM.cmake
@@ -24,7 +24,7 @@ ExternalProject_Add(${_proj_name}
   STAMP_DIR         ${_SB_BINARY_DIR}/stamp
   #--Download step--------------
   DOWNLOAD_DIR      ${SB_DOWNLOAD_DIR}
-  GIT_REPOSITORY    https://github.com/OpenDroneMap/OpenSfM/
+  GIT_REPOSITORY    https://github.com/jadsonbr/OpenSfM/
   GIT_TAG           291
   #--Update/Patch step----------
   UPDATE_COMMAND    git submodule update --init --recursive
